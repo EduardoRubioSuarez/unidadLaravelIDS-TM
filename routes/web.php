9198d0e43bfce4430de73dc317c6c908c607e877
EduardoRubio <rubioeduardo41@gmail.com>
2024-11-19 11:19:15 -0700

    Correccion en branch

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,30 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/saludar/{name}/{lastname?}', function ($name,$lastname = "Doe") {
+    return 'welcome '. $name .' '. $lastname;
+});
+
+Route::get('/suma/{n1}/{n2}', function ($n1,$n2) {
+    $resultado = $n1 + $n2;
+    return 'El resultado es: '. $resultado;
+});
+
+Route::get('/resta/{n1}/{n2}', function ($n1,$n2) {
+    $resultado = $n1 - $n2;
+    return 'El resultado es: '. $resultado;
+});
+
+Route::get('/multi/{n1}/{n2}', function ($n1,$n2) {
+    $resultado = $n1 * $n2;
+    return 'El resultado es: '. $resultado;
+});
+
+Route::get('/div/{n1}/{n2}', function ($n1,$n2) {
+    $resultado = $n1 / $n2;
+    return 'El resultado es: '. $resultado;
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
