a778162aeee034e5ec2be8e808c8b3e53b636ffd
EduardoRubio <rubioeduardo41@gmail.com>
2024-11-19 11:07:08 -0700

    Act 7

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,33 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/saludar/{name}/{lastname?}', function ($name,$lastname = "Doe") {
+    return 'welcome '. $name .' '. $lastname;
+});
+
+Route::get('/suma/{n1}/{n2}', function ($n1,$n2) {
+    $resultado = $n1 + $n2;
+    return 'El resultado es: '. $resultado;
+});
+
+Route::get('/resta/{n1}/{n2}', function ($n1,$n2) {
+    $resultado = $n1 - $n2;
+    return 'El resultado es: '. $resultado;
+});
+
+Route::get('/multi/{n1}/{n2}', function ($n1,$n2) {
+    $resultado = $n1 * $n2;
+    return 'El resultado es: '. $resultado;
+});
+
+Route::get('/div/{n1}/{n2}', function ($n1,$n2) {
+    $resultado = $n1 / $n2;
+    return 'El resultado es: '. $resultado;
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
